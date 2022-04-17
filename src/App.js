7a469bacffb5c0c5fb74d85ae8bc0c4605455849
H_A _Opu <wwwhasib4@gmail.com>
2022-04-17 10:03:35 +0600

    install tailwind and create bunch of component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Login from "./Pages/Login/Login";
+import Header from "./Pages/Shared/Header/Header";
+import Footer from "./Pages/Shared/Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
