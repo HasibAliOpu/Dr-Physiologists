133e49cf4c7b5553445682d16583af1b3dbe8f5d
H_A _Opu <wwwhasib4@gmail.com>
2022-04-17 14:43:16 +0600

    protect the firebase config

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,8 +4,12 @@ import Logo from "../../../images/icon/logo.png";
 const Header = () => {
   return (
     <div>
-      <div className="bg-teal-300 flex items-center justify-between px-8">
-        <img src={Logo} className="w-16 h-16" alt="" />
+      <div className=" flex items-center justify-between px-8">
+        <span className="flex items-center">
+          <img src={Logo} className="w-14 h-14" alt="" />
+          <h1 className="text-xl text-blue-500">Dr Physiologists</h1>
+        </span>
+
         <div className="flex font-medium mr-20">
           <CustomLink to="/">Home</CustomLink>
           <CustomLink to="/about">About</CustomLink>
