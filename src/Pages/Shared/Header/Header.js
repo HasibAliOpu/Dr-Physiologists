7d859e8a190395f2bdd44a879d0c4e8e53282418
H_A _Opu <wwwhasib4@gmail.com>
2022-04-17 10:43:22 +0600

    navbar added using custom active link

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import CustomLink from "../../../CustomLink/CustomLink";
+import Logo from "../../../images/icon/logo.png";
 const Header = () => {
   return (
     <div>
-      <h1>This is Header</h1>
+      <div className="bg-teal-300 flex items-center justify-between px-8">
+        <img src={Logo} className="w-16 h-16" alt="" />
+        <div className="flex font-medium mr-20">
+          <CustomLink to="/">Home</CustomLink>
+          <CustomLink to="/about">About</CustomLink>
+          <CustomLink to="/blogs">Blogs</CustomLink>
+          <CustomLink to="/login">Login</CustomLink>
+        </div>
+      </div>
     </div>
   );
 };
